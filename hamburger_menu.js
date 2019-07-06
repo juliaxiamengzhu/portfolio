05d758902e86437f9339893fb93073c3d2814ddb
Julia <juliazhu2413@gmail.com>
2019-07-05 21:09:43 -0400

    Used javascript to create responsive menu on small screens

---
+++ hamburger_menu.js
@@ -0,0 +1,18 @@
+var hamburger = document.getElementById("hamburger-icon");
+var overlay = document.getElementById("overlay");
+var menu = document.querySelector("nav ul");
+
+hamburger.onclick = function(){
+	if (overlay.style.display === "none"){
+		overlay.style.display = "block";
+		menu.style.display = "block";
+	} else{
+		overlay.style.display = "none";
+		menu.style.display = "none";
+	}
+}
+
+overlay.onclick = function(){
+	overlay.style.display = "none";
+	menu.style.display = "none";
+}
