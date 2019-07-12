5c374dd5fa5b17d8a99cb594eb2e5a957167e865
Julia <juliazhu2413@gmail.com>
2019-07-11 20:12:29 -0400

    hamburger menu functions now.

--- hamburger_menu.js
+++ hamburger_menu.js
@@ -1,18 +1,22 @@
 var hamburger = document.getElementById("hamburger-icon");
 var overlay = document.getElementById("overlay");
-var menu = document.querySelector("nav ul");
+var menu = document.getElementById("menu");
 
+//Function for opening and closing menu 
 hamburger.onclick = function(){
-	if (menu.style.display === "none"){
+	if (menu.id == "menu"){
+		menu.id = "menu-responsive";
 		overlay.style.display = "block";
-		menu.style.display = "block";
-	} else{
+	} else {
+		menu.id = "menu";
 		overlay.style.display = "none";
-		menu.style.display = "none";
 	}
 }
 
-overlay.onclick = function(){
-	overlay.style.display = "none";
-	menu.style.display = "none";
-}
+//function for closing menu when click on the overlay
+window.onclick = function(event) {
+  if (event.target == overlay) {
+    overlay.style.display = "none";
+    menu.id = "menu";
+  }
+} 
