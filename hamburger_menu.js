d49a4363294227f02ea8f43073a9d8835817dbb3
Julia <juliazhu2413@gmail.com>
2019-07-12 22:57:05 -0400

    changed advertisement_showpiece filename to avoid adblock. Tweaked breakpoints and added color to hamburger icon when clicked.

--- hamburger_menu.js
+++ hamburger_menu.js
@@ -5,9 +5,11 @@ var menu = document.getElementById("menu");
 //Function for opening and closing menu 
 hamburger.onclick = function(){
 	if (menu.id == "menu"){
+		hamburger.style.color = "#BC4C3F";
 		menu.id = "menu-responsive";
 		overlay.style.display = "block";
 	} else {
+		hamburger.style.color = "#828282";
 		menu.id = "menu";
 		overlay.style.display = "none";
 	}
@@ -16,6 +18,7 @@ hamburger.onclick = function(){
 //function for closing menu when click on the overlay
 window.onclick = function(event) {
   if (event.target == overlay) {
+  	hamburger.style.color = "#828282";
     overlay.style.display = "none";
     menu.id = "menu";
   }
